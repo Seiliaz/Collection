b8b0a0f9ac3b5367c7bf344baeb10bf5c9ac8de0
Seiliaz <seiliaz@yahoo.com>
2024-01-01 23:08:21 +0330

    final commit before push

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ type Collection struct {
 }
 
 func constructor() Collection {
-	return Collection{latestIndex: 0}
+	return Collection{latestIndex: 0, data: make(map[string]string)}
 }
 
-func (collection Collection) Append(data ...string) {
+func (collection *Collection) Append(data ...string) {
 	var key string
 	var value string
 	if len(data) == 1 {
 		key = fmt.Sprint(collection.latestIndex)
 		value = data[0]
+		collection.latestIndex++
 	} else if len(data) >= 2 {
 		key = data[0]
 		value = data[1]
@@ -25,13 +26,11 @@ func (collection Collection) Append(data ...string) {
 	if ok {
 		fmt.Println("the key is already taken and the value is", val)
 	} else {
-		fmt.Println("there we are!")
-		// collection.data[key] = value
+		collection.data[key] = value
 	}
 }
 
 func main() {
 	myCollection := constructor()
 	myCollection.Append("hello")
-	fmt.Println(myCollection.data)
 }
